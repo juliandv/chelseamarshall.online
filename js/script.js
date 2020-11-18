9f3e3f91919b38297bddc42e6650cd53918fbab6
julian <juliandv96@gmail.com>
2020-11-18 21:32:51 +1300

    Add header, add body portfolio cards

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleHeaderNav() {
+  var x = document.getElementById("header-accordian-content");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
